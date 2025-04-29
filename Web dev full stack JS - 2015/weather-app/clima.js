4ec6c5045e9c3bf994a2c464b520e18a8ea8044c
Francisco Fonseca <franfonseca8594@gmail.com>
2025-04-29 19:08:32 -0300

    first commit

---
+++ Web dev full stack JS - 2015/weather-app/clima.js
@@ -0,0 +1,57 @@
+#!/usr/local/bin/node
+
+var request = require('request');
+var colors = require("colors");
+var geoip = require('geoip-lite');
+
+var APPID="425f301ab92c67d8336f2dbb5f82c4e1";
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function processResponse(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var bodyObj = JSON.parse(body);
+    console.log("Temperatura en "+bodyObj.name+": "+bodyObj.main.temp.toString().red+' ºC');
+    console.log("Min/Max: "+bodyObj.main.temp_min+'/'+bodyObj.main.temp_max);
+    console.log("Humedad: "+bodyObj.main.humidity.toString().green+'%');
+    console.log(capitalizeFirstLetter(bodyObj.weather[0].description));
+  }else{
+    console.log(error);
+  }
+}
+
+function getMyIp(cb){
+  function rqCB(err, response, body){
+    var ip=JSON.parse(body).ip;
+    cb(ip);
+  }
+  request('https://api.ipify.org/?format=json', rqCB);
+}
+
+
+function giveCity(ciudad){
+  var url='http://api.openweathermap.org/data/2.5/weather?q='+ciudad+'&APPID='+APPID+"&units=metric&lang=es";
+    request(url, processResponse);
+}
+
+function init(){
+  var ciudad=process.argv[2];
+
+  if(!ciudad) {
+    getMyIp(function(miip){
+      var geo = geoip.lookup(miip);
+      ciudad = geo.city || "Buenos Aires";
+      giveCity(ciudad);
+    });
+  } else {
+    giveCity(ciudad);
+    }
+    
+}
+
+
+
+init();
+
---
+++ Web dev full stack JS - 2015/weather-app/index.js
@@ -0,0 +1,21 @@
+var prueba, var num1, var num2, var r;
+
+//prueba condicion
+
+var prueba = "Estoy intentando codificar algo."
+	console.log(prueba);
+
+num1 = parseInt(prompt("Valor Numero 1"));
+num2 = parseInt(prompt("Valor Numero 2"));
+
+r = num1+num2;
+
+	if(r >= 10){
+		console.log("El resultado es mayor de 10");
+	}
+	else{
+		console.log("El resultado es menor de 10");
+	}
+
+
+
